ce98709769040e1bc4b921f0d4998567e041e6a0
Michael Than <mthan@MichaelTs-MacBook-Pro.local>
2022-12-01 12:48:25 -0800

    day 1

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut max: u32 = 0;
+    let mut counter: u32 = 0;
+
+    let file = File::open("input.txt")?;
+    let lines = io::BufReader::new(file).lines();
+
+    for line_result in lines {
+        let line: String = line_result?;
+        if line == "" {
+            counter = 0;
+            continue
+        }
+        let value = line.parse::<u32>()?;
+        counter += value;
+        if counter > max {
+            max = counter
+        }
+    }
+    println!("{}", max);
+    return Ok(());
+}

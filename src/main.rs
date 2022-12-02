7150a2fc6fb1574eb0ad16d157ca96a06306b95e
Michael Than <mthan@MichaelTs-MacBook-Pro.local>
2022-12-02 10:07:00 -0800

    day 1 part 2

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,35 @@
 use std::fs::File;
 use std::io::{self, BufRead};
+use std::collections::BinaryHeap;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut max: u32 = 0;
-    let mut counter: u32 = 0;
+    let mut max:i32 = 0;
+    let mut counter: i32 = 0;
 
     let file = File::open("input.txt")?;
     let lines = io::BufReader::new(file).lines();
+    let mut top3: BinaryHeap<i32> = BinaryHeap::new();
 
     for line_result in lines {
         let line: String = line_result?;
         if line == "" {
+            top3.push(counter * -1);
+            if top3.len() > 3 {
+                top3.pop();
+            }
             counter = 0;
             continue;
         }
-        let value = line.parse::<u32>()?;
+        let value = line.parse::<i32>()?;
         counter += value;
         if counter > max {
             max = counter
         }
     }
-    println!("{}", max);
+
+    let mut sum: i32 = 0;
+    while top3.len() > 0 {
+        sum += top3.pop().unwrap() * -1;
+    }
+    println!("{}", sum);
     return Ok(());
 }
